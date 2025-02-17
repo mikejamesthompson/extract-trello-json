8ab03f36e55b673640b431983db5394b0526e03f
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-02-17 16:16:05 +0000

    Record and display file sizes as they're sent

--- attachment_server.py
+++ attachment_server.py
@@ -1,16 +1,26 @@
 from flask import Flask, send_from_directory, abort
 import os
-
+import humanize
 app = Flask(__name__)
 
-@app.route("/<path:filename>")
-def serve_attachment(filename: str):
+TOTAL_FILE_SIZE = 0
+
+@app.route("/<path:file_name>")
+def serve_attachment(file_name: str):
     """
     Serves the image file from the attachments directory.
     """
+
+    file_path = os.path.join(os.getenv("ATTACHMENT_DIRECTORY"), file_name)
+
     try:
+        file_size = os.path.getsize(file_path)
+        global TOTAL_FILE_SIZE
+        TOTAL_FILE_SIZE += file_size
+        print(f"👇 Following file's size: {humanize.naturalsize(file_size)} --- Total file size sent: {humanize.naturalsize(TOTAL_FILE_SIZE)}")
+
         # The send_from_directory helper safely serves files.
-        return send_from_directory(os.getenv("ATTACHMENT_DIRECTORY"), filename)
+        return send_from_directory(os.getenv("ATTACHMENT_DIRECTORY"), file_name)
     except FileNotFoundError:
         abort(404)
 
