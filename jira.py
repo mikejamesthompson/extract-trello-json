feb556ed3b62270b82c37ce0805cbdc7c4253c64
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-02-17 13:54:28 +0000

    Increase workers

--- jira.py
+++ jira.py
@@ -73,7 +73,7 @@ def process_card(card):
 # Parallelize processing with ThreadPoolExecutor.
 def process_all_cards(trello_cards):
     jira_cards = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         # executor.map preserves the order of the input list.
         for result in tqdm(executor.map(process_card, trello_cards), total=len(trello_cards)):
             jira_cards.append(result)
